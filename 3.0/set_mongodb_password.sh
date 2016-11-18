3af2a9a669edef029055bb6f7a2b1ae3430d0621
GloriaPG <ing.gloriapalmagonzalez@gmail.com>
2016-11-18 14:05:51 -0600

    More parameters

--- 3.0/set_mongodb_password.sh
+++ 3.0/set_mongodb_password.sh
@@ -3,16 +3,25 @@
 USER=${MONGODB_USER:-"admin"}
 DATABASE=${MONGODB_DATABASE:-"admin"}
 PASS=${MONGODB_PASS:-$(pwgen -s 12 1)}
-_word=$( [ ${MONGODB_PASS} ] && echo "preset" || echo "random" )
 
-RET=1
-while [[ RET -ne 0 ]]; do
-    echo "=> Waiting for confirmation of MongoDB service startup"
-    sleep 5
-    mongo admin --eval "help" >/dev/null 2>&1
-    RET=$?
-done
+set -f                      # avoid globbing (expansion of *).
+array=(${USER//:/ })
+array_database=(${DATABASE//:/ })
+array_pass=(${PASS//:/ })
+for i in "${!array[@]}"
+do
+    USER=${array[i]:-"admin"}
+    DATABASE=${array_database[i]:-"admin"}
+    PASS=${array_pass[i]:-$(pwgen -s 12 1)}
+    _word=$( [ ${MONGODB_PASS} ] && echo "preset" || echo "random" )
 
+    RET=1
+    while [[ RET -ne 0 ]]; do
+        echo "=> Waiting for confirmation of MongoDB service startup"
+        sleep 5
+        mongo admin --eval "help" >/dev/null 2>&1
+        RET=$?
+    done
 echo "=> Creating an ${USER} user with a ${_word} password in MongoDB"
 mongo admin --eval "db.createUser({user: '$USER', pwd: '$PASS', roles:[{role:'root',db:'admin'}]});"
 
@@ -23,6 +32,7 @@ use $DATABASE
 db.createUser({user: '$USER', pwd: '$PASS', roles:[{role:'dbOwner',db:'$DATABASE'}]})
 EOF
 fi
+done
 
 echo "=> Done!"
 touch /data/db/.mongodb_password_set
